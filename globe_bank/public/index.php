2c0863f121119e0cf29e9409d2afad600ff77f18
ivanhack34 <ivanpolanco34@gmail.com>
2022-10-13 20:13:33 -0400

    Primera parte del curso hast ahora

---
+++ globe_bank/public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Globe bank</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+
+  <h1>Globe Bank: Coming Soon</h1>
+
+  </body>
+</html>
