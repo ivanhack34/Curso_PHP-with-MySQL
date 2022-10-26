e0d2938e563cd786a285d36b91fbd1c84a6e8e2e
ivanhack34 <ivanpolanco34@gmail.com>
2022-10-26 07:34:17 -0400

    Actualizacion del curso con el cap 5, 6 y 7

---
+++ globe_bank/private/db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "127.0.0.1:3308"); /*Siempre poner el servidor que funcione*/
+define("DB_USER", "root"); /*Poner el numero de usuario correcto*/ 
+define("DB_PASS", ""); 
+define("DB_NAME", "globe_bank"); /*Poner la base de datos correcta */
+
+?>
--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -2,7 +2,6 @@
 
 ob_start(); // output buffering is turned on
 
-    require_once('functions.php');
 
  // Assign file paths to PHP constants
 // __FILE__ returns the current path to this file
@@ -22,6 +21,12 @@ define("SHARED_PATH", PRIVATE_PATH . '/shared');
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
+
+require_once('functions.php');
+require_once('database.php');
+require_once('query_functions.php');
+
+$db = db_connect();
 
 
 ?>
